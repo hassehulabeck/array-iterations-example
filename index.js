3e094e6694a340a4fc1bf9e13392a117d2f653ac
hassehulabeck <hansbudandersson@gmail.com>
2019-10-17 13:11:46 +0200

    First commit, added persons

---
+++ index.js
@@ -0,0 +1,35 @@
+var persons = [{
+        name: "Adam",
+        age: 34,
+        hobbies: [
+            "Fishing",
+            "Frimärkssamling"
+        ]
+    },
+    {
+        name: "Sofia",
+        age: 64,
+        hobbies: [
+            "Walking",
+            "socializing",
+            "collecting stamps"
+        ]
+    },
+    {
+        name: "Ulf",
+        age: 27,
+        hobbies: []
+    },
+    {
+        name: "Ibrahim",
+        age: 53,
+        hobbies: [
+            "sports", "computers", "movies"
+        ]
+    },
+    {
+        name: "Flo",
+        age: 49,
+        hobbies: ["Norwegian literature"]
+    }
+]
